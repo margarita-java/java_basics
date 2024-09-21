b817e19ed0369d7d3fe3faf5261faf5a0efe4d4d
Rita <r_arutkina@mail.ru>
2024-09-21 16:32:34 +0900

    Completed the tasks

--- ObjectsAndClasses_Methods/src/Main.java
+++ ObjectsAndClasses_Methods/src/Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         Basket basket = new Basket();
-        basket.add("Milk", 40, 5,8);
+        basket.add("Milk", 40, 5,2);
+        basket.add("Water", 20, 2,1.3);
+        basket.print("Milk, Water");
 
         System.out.println("Общий вес товаров в корзине ");
         System.out.println(basket.getTotalWeight());

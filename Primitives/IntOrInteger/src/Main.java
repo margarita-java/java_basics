e155d2ab4ec3803a85f9b4c040c2a029af077a67
Rita <r_arutkina@mail.ru>
2024-10-14 10:06:31 +0900

    Completed the task.

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -8,5 +8,20 @@ public class Main {
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
+        for (char c = 'А'; c <= 'Е'; c++) {
+            if ( c >= 'А' &&  c <= 'Е');
+            System.out.println( (int) c + " - " + c);
+        }
+        System.out.println( (int) 'Ё' + " - " + 'Ё');
+        for (char c = 'Ж'; c <= 'е'; c++) {
+            if ( c >= 'Ж' &&  c <= 'е');
+            System.out.println( (int) c + " - " + c);
+        }
+        System.out.println( (int) 'ё' + " - " + 'ё');
+        for (char c = 'ж'; c <= 'я'; c++) {
+            if ( c >= 'ж' &&  c <= 'я');
+            System.out.println( (int) c + " - " + c);
+        }
+
     }
 }

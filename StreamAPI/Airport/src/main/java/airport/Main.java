107f1a672bfa401a70005811a2763e1717ce2105
Rita <r_arutkina@mail.ru>
2024-11-19 11:08:15 +0900

    Implemented methods using the Stream API and lambda expressions.The tests were passed locally.

--- StreamAPI/Airport/src/main/java/airport/Main.java
+++ StreamAPI/Airport/src/main/java/airport/Main.java
@@ -2,29 +2,31 @@ package airport;
 
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+import com.skillbox.airport.Flight.Type;
 
+import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        Airport airport =Airport.getInstance();
+        Airport airport = Airport.getInstance();
     }
 
     public static long findCountAircraftWithModelAirbus(Airport airport, String model) {
-
-        airport.getAllAircraft()
+        return airport.getAllAircrafts()
                 .stream()
-                .filter(aircraft.getModel().startWich(model)
-                .count()
+                .filter(aircraft -> aircraft.getModel().startsWith(model))
+                .count();
     }
 
     public static Map<String, Integer> findMapCountParkedAircraftByTerminalName(Airport airport) {
-
-        airport.getTerminals()
-                .stream
+        return airport.getTerminals()
+                .stream()
                 .collect(Collectors.toMap(
                         terminal -> terminal.getName(),
                         terminal -> terminal.getParkedAircrafts().size()
@@ -32,12 +34,10 @@ public class Main {
     }
 
     public static List<Flight> findFlightsLeavingInTheNextHours(Airport airport, int hours) {
-
         Instant now = Instant.now();
         Instant upperBound = now.plusSeconds(hours * 3600);
-
         return airport.getTerminals()
-                .stream
+                .stream()
                 .flatMap(terminal -> terminal.getFlights().stream())
                 .filter(flight -> flight.getType() == Type.DEPARTURE)
                 .filter(flight -> flight.getDate().isAfter(now) && flight.getDate().isBefore(upperBound))
@@ -45,13 +45,11 @@ public class Main {
     }
 
     public static Optional<Flight> findFirstFlightArriveToTerminal(Airport airport, String terminalName) {
-
         return airport.getTerminals()
-                .stream
+                .stream()
                 .filter(terminal -> terminal.getName().equals(terminalName))
-                .flatMap(terminal -> terminal.getFlights().stream()
+                .flatMap(terminal -> terminal.getFlights().stream())
                 .filter(flight -> flight.getType() == Type.ARRIVAL)
-                        .min(Comparator.comparing(Flight::getDate));
+                .min(Comparator.comparing(Flight::getDate));
     }
-
 }

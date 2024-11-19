1fd0af68ea50ed326ad9c9aa85df576d4e60f599
Rita <r_arutkina@mail.ru>
2024-11-19 10:40:30 +0900

    Implemented methods using the Stream API and lambda expressions.

--- StreamAPI/Airport/src/main/java/airport/Main.java
+++ StreamAPI/Airport/src/main/java/airport/Main.java
@@ -9,25 +9,49 @@ import java.util.Map;
 import java.util.Optional;
 
 public class Main {
+    public static void main(String[] args) {
+        Airport airport =Airport.getInstance();
+    }
 
     public static long findCountAircraftWithModelAirbus(Airport airport, String model) {
-        //TODO Метод должен вернуть количество самолетов указанной модели.
-        // подходят те самолеты, у которых name начинается со строки model
-        return 0;
+
+        airport.getAllAircraft()
+                .stream()
+                .filter(aircraft.getModel().startWich(model)
+                .count()
     }
 
     public static Map<String, Integer> findMapCountParkedAircraftByTerminalName(Airport airport) {
-        //TODO Метод должен вернуть словарь с количеством припаркованных самолетов в каждом терминале.
-        return Collections.emptyMap();
+
+        airport.getTerminals()
+                .stream
+                .collect(Collectors.toMap(
+                        terminal -> terminal.getName(),
+                        terminal -> terminal.getParkedAircrafts().size()
+                ));
     }
 
     public static List<Flight> findFlightsLeavingInTheNextHours(Airport airport, int hours) {
-        //TODO Метод должен вернуть список отправляющихся рейсов в ближайшее количество часов.
-        return Collections.emptyList();
+
+        Instant now = Instant.now();
+        Instant upperBound = now.plusSeconds(hours * 3600);
+
+        return airport.getTerminals()
+                .stream
+                .flatMap(terminal -> terminal.getFlights().stream())
+                .filter(flight -> flight.getType() == Type.DEPARTURE)
+                .filter(flight -> flight.getDate().isAfter(now) && flight.getDate().isBefore(upperBound))
+                .collect(Collectors.toList());
     }
 
     public static Optional<Flight> findFirstFlightArriveToTerminal(Airport airport, String terminalName) {
-        //TODO Найти ближайший прилет в указанный терминал.
-        return Optional.empty();
+
+        return airport.getTerminals()
+                .stream
+                .filter(terminal -> terminal.getName().equals(terminalName))
+                .flatMap(terminal -> terminal.getFlights().stream()
+                .filter(flight -> flight.getType() == Type.ARRIVAL)
+                        .min(Comparator.comparing(Flight::getDate));
     }
+
 }

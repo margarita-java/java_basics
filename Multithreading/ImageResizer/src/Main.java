da34ea6f399e45c9b396cd01e7d7bbe69946d12f
r_aru <r_arutkina@mail.ru>
2025-01-30 12:17:52 +0900

    I learned how to work with the Thread class

--- Multithreading/ImageResizer/src/Main.java
+++ Multithreading/ImageResizer/src/Main.java
@@ -3,49 +3,43 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class Main {
-
     public static void main(String[] args) {
-        String srcFolder = "/users/sortedmap/Desktop/src";
-        String dstFolder = "/users/sortedmap/Desktop/dst";
+        String srcFolder = "C:\\Users\\r_aru\\Desktop\\src";
+        String dstFolder = "C:\\Users\\r_aru\\Desktop\\dst";
 
         File srcDir = new File(srcFolder);
-
-        long start = System.currentTimeMillis();
-
         File[] files = srcDir.listFiles();
 
-        try {
-            for (File file : files) {
-                BufferedImage image = ImageIO.read(file);
-                if (image == null) {
-                    continue;
-                }
-
-                int newWidth = 300;
-                int newHeight = (int) Math.round(
-                    image.getHeight() / (image.getWidth() / (double) newWidth)
-                );
-                BufferedImage newImage = new BufferedImage(
-                    newWidth, newHeight, BufferedImage.TYPE_INT_RGB
-                );
-
-                int widthStep = image.getWidth() / newWidth;
-                int heightStep = image.getHeight() / newHeight;
-
-                for (int x = 0; x < newWidth; x++) {
-                    for (int y = 0; y < newHeight; y++) {
-                        int rgb = image.getRGB(x * widthStep, y * heightStep);
-                        newImage.setRGB(x, y, rgb);
-                    }
-                }
-
-                File newFile = new File(dstFolder + "/" + file.getName());
-                ImageIO.write(newImage, "jpg", newFile);
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        if (files == null || files.length == 0) {
+            System.out.println("Папка с изображениями пуста или не существует.");
+            return;
         }
 
-        System.out.println("Duration: " + (System.currentTimeMillis() - start));
+        int numCores = Runtime.getRuntime().availableProcessors();
+        int filesPerThread = (int) Math.ceil(files.length / (double) numCores);
+
+        long start = System.currentTimeMillis();
+        Thread[] threads = new Thread[numCores];
+
+        for (int i = 0; i < numCores; i++) {
+            int startIdx = i * filesPerThread;
+            int endIdx = Math.min(startIdx + filesPerThread, files.length);
+
+            File[] filesSubset = new File[endIdx - startIdx];
+            System.arraycopy(files, startIdx, filesSubset, 0, filesSubset.length);
+
+            threads[i] = new Thread(new ImageResizer(filesSubset, dstFolder));
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Total Duration: " + (System.currentTimeMillis() - start) + " ms");
     }
 }
